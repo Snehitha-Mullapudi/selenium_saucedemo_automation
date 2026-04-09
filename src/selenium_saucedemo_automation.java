20063802ea4968e7933818bce2af5adecdd5bfb5
Snehitha Mullapudi <snehithamullapudi@gmail.com>
2026-04-09 12:55:20 +0530

    Initial Selenium SauceDemo project

---
+++ src/selenium_saucedemo_automation.java
@@ -0,0 +1,76 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.openqa.selenium.By;
+
+public class selenium_saucedemo_automation {
+
+
+		    public static void main(String[] args) throws InterruptedException {
+
+		        // Launch browser
+		        WebDriver driver = new ChromeDriver();
+		        driver.get("https://www.saucedemo.com/");
+
+		        // ---------------- LOGIN ----------------
+		        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		        driver.findElement(By.id("login-button")).click();
+
+		        // Verify login
+		        String title = driver.findElement(By.className("app_logo")).getText();
+		        System.out.println("Page Title: " + title);
+		        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='app_logo']")).getText(), "Swag Labs");
+
+		        // ---------------- SELECT PRODUCT ----------------
+		        driver.findElement(By.className("inventory_item_name")).click();
+
+		        // Print product details
+		        System.out.println(driver.findElement(By.className("inventory_details_desc")).getText());
+
+		        // Add to cart
+		        driver.findElement(By.id("add-to-cart")).click();
+
+		        // ---------------- CART ----------------
+		        driver.findElement(By.className("shopping_cart_link")).click();
+
+		        System.out.println(driver.findElement(By.className("cart_quantity_label")).getText());
+		        System.out.println(driver.findElement(By.className("cart_item")).getText());
+
+		        // Continue shopping
+		        driver.findElement(By.id("continue-shopping")).click();
+
+		        // Go back to cart
+		        driver.findElement(By.className("shopping_cart_link")).click();
+
+		        // ---------------- CHECKOUT ----------------
+		        driver.findElement(By.id("checkout")).click();
+
+		        driver.findElement(By.id("first-name")).sendKeys("Snehitha");
+		        driver.findElement(By.id("last-name")).sendKeys("Mullapudi");
+		        driver.findElement(By.id("postal-code")).sendKeys("177565");
+
+		        driver.findElement(By.id("continue")).click();
+
+		        System.out.println(driver.findElement(By.id("checkout_summary_container")).getText());
+
+		        // Finish order
+		        driver.findElement(By.id("finish")).click();
+
+		        // Verify order success
+		        String confirmation = driver.findElement(By.className("complete-header")).getText();
+		        System.out.println("Order Status: " + confirmation);
+
+		        // ---------------- LOGOUT ----------------
+		        driver.findElement(By.id("react-burger-menu-btn")).click();
+		        Thread.sleep(2000); // will replace with waits later
+		        driver.findElement(By.id("logout_sidebar_link")).click();
+
+		        // Close browser
+		        driver.quit();
+		    
+		
+
+	}
+
+}
